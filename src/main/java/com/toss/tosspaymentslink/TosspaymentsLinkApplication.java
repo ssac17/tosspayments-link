d0327e364d7fbd91556ff907f79273de28db5ca3
ssac17 <84308365+ssac17@users.noreply.github.com>
2026-07-23 12:27:13 +0900

    Initial commit

---
+++ src/main/java/com/toss/tosspaymentslink/TosspaymentsLinkApplication.java
@@ -0,0 +1,13 @@
+package com.toss.tosspaymentslink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TosspaymentsLinkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TosspaymentsLinkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/toss/tosspaymentslink/TosspaymentsLinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toss.tosspaymentslink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TosspaymentsLinkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
